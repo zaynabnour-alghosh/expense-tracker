060c22fe496a77e33c14e6403227c542e3a67039
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-07-19 01:32:24 +0300

    remove expense and update total expenses

--- src/script.js
+++ src/script.js
@@ -1,17 +1,17 @@
 const alertext=$("#txt") ;
 let total_exp=0;
 function exp_amt_element(expense,amount) {
-    if ((expense=="" ||amount=="")){
+    if ((expense=="" ||amount=="" ||isNaN(amount))){
         alertext.show(); 
+        $("$total-exp").html('$ '+total_exp);
         return;
     }
     else{
         alertext.hide();
         return `<tr class=new-row>
-        <th>${expense}</th>
-        <th>$ ${amount}</th>
-        <th>
-        <span class="edit">&#9998;</span>
+        <th class=exp>${expense}</th>
+        <th class=amnt>$ ${amount}</th>
+        <th>        
           <span class="remove">&#128465</span>
         </th>
         </tr>`
@@ -31,21 +31,34 @@ function addExpense() {
     // console.log(exp.val());
     // console.log(amt.val());    
    
-        const exp_row= $(exp_amt_element(exp.val(),amt.val()));
-        let exp_val=parseInt(amt.val());
-        console.log(exp_val);
-        total_exp=total_exp+parseInt(exp_val);
-        console.log(total_exp);
-        texp.append(exp_row);
-        amt.val("");
-        exp.val("");
+    const exp_row= $(exp_amt_element(exp.val(),amt.val()));
+    console.log(exp_row);
+    let exp_val=parseInt(amt.val());
+    // console.log(exp_val);
+    total_exp=total_exp+parseInt(exp_val);
+    console.log(total_exp);
+    texp.append(exp_row);
+    amt.val("");
+    exp.val("");
+   
+    // remove expense
+
+    exp_row.find(".remove").click(function(){
+    //    console.log(this.val);
         $("#total-exp").html('$ '+total_exp);
-
-
+        exp_row.remove();
+        total_exp=parseInt(total_exp)-parseInt(exp_val);
+        
+        // console.log(total_exp);
+        $("#total-exp").html('$ '+total_exp);
+    })       
+    $("#total-exp").html('$ '+total_exp);
 }
+
 
 $(document).ready(function () {   
     const addButton = $("#btnAdd");    
     addButton.click(addExpense);
     console.log("hi");
+   
   })
